ec20961eb0ee32f796c1402aa91509134c36871c
trevor knowles <knowlet1@gmatc.matc.edu>
2022-10-28 17:20:42 -0500

    Completed Activity 7

--- blog/src/components/layout.js
+++ blog/src/components/layout.js
@@ -1,20 +1,13 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/use-static-query/
- */
-
 import * as React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-import styled, { ThemeProvider } from "styled-components"
+import styled, { ThemeProvider } from 'styled-components'
 
-import { Gray } from "./themes/Gray"
+import { Gray } from './themes/Gray'
 import { Header } from "./Header"
 import "./layout.css"
-import { Main } from "./Main"
-import { Footer } from "./Footer"
+import { Main } from './Main'
+import { Footer } from './Footer'
 
 const Content = styled.div`
   margin: 0 auto;
